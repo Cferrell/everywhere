1e3329e20eabf511519d33341cd80e6a4d905fb0
CFerrell <cferrell@rogers.com>
2017-07-08 22:03:59 -0400

    remote template

---
+++ wp-content/themes/safreen-child/page-remoteyear.php
@@ -0,0 +1,23 @@
+<!--This is the page to display the map on-->
+<?php /* Template Name: RemoteYear */ ?>
+
+
+<?php get_header(); ?>
+
+<!-- head select -->
+
+     <?php get_template_part('headers/part','headsingle'); ?>
+<!-- / head select
+
+<div id="sub_banner">
+<h1>
+<?php the_title(); ?>
+</h1>
+<div class='h-line'></div>
+
+</div>-->
+
+<!--Content-->
+<h2> What is Remote Year!?</h2>
+
+<?php get_footer(); ?>
